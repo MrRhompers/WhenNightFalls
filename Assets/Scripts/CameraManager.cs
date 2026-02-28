703528deca4a6206ca4353363c91d064293d31d7
Olayanka Reddy <olayankar@gmail.com>
2026-02-28 15:01:14 +0200

    Camera SetUp
    We finally did something XD

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera[] otherCameras;
+
+    private Camera currentCamera;
+
+    void Start()
+    {
+        SwitchCamera(mainCamera);
+    }
+
+    public void SwitchToCamera(int cameraIndex)
+    {
+        if (cameraIndex < 0 || cameraIndex >= otherCameras.Length)
+            return;
+
+        SwitchCamera(otherCameras[cameraIndex]);
+    }
+
+    public void SwitchToMainCamera()
+    {
+        SwitchCamera(mainCamera);
+    }
+
+    void SwitchCamera(Camera newCam)
+    {
+        if (currentCamera == newCam) return;
+
+        mainCamera.enabled = false;
+
+        foreach (Camera cam in otherCameras)
+            cam.enabled = false;
+
+        newCam.enabled = true;
+        currentCamera = newCam;
+    }
+}
